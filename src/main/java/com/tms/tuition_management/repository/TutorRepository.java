fe35d57b0b3d913c352debe73f6c9aa24d26d250
IT24101261B <bhanukakck2020@gmail.com>
2025-10-24 11:27:34 +0530

    Updated by IT24101261 TutorRepository.java

--- src/main/java/com/tms/tuition_management/repository/TutorRepository.java
+++ src/main/java/com/tms/tuition_management/repository/TutorRepository.java
@@ -2,7 +2,7 @@ package com.tms.tuition_management.repository;
 
 import com.tms.tuition_management.model.Tutor;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional; // Ensure Optional is imported
+import java.util.Optional; 
 
 public interface TutorRepository extends JpaRepository<Tutor, Long> {
 
