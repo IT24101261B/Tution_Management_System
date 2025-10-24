47140078dc645268c9950f7fc3f74cea9aac6e14
IT24101261B <bhanukakck2020@gmail.com>
2025-10-24 11:33:56 +0530

    Updated by IT24101261 ParentService.java

--- src/main/java/com/tms/tuition_management/service/ParentService.java
+++ src/main/java/com/tms/tuition_management/service/ParentService.java
@@ -5,17 +5,15 @@ import java.util.List;
 
 public interface ParentService {
 
-    // --- Add these missing methods ---
     List<Parent> getAllParents();
 
     Parent getParentById(Long id);
 
-    void saveParent(Parent parent); // Can be used for both create and update
+    void saveParent(Parent parent); 
 
     void deleteParentById(Long id);
 
     void updateParentStudents(Parent parent, List<Long> studentIds);
-    // --- End of added methods ---
 
-    Parent findByUserId(Long userId); // Existing method if you have it
+    Parent findByUserId(Long userId); 
 }
